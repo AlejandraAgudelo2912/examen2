e7cdf78a755d461a12f3aad10d10b7936d06b253
LAPTOP-1DQNJ8L3\aleja <339210@alu.murciaeduca.es>
2024-12-19 16:25:49 +0100

    Ejercicio 4

--- app/Http/Controllers/Api/V3/SubcategoryController.php
+++ app/Http/Controllers/Api/V3/SubcategoryController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api\V3;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\SubcategoryResource;
+use App\Models\Subcategory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class SubcategoryController extends Controller
@@ -10,5 +12,12 @@ class SubcategoryController extends Controller
     public function index()
     {
 
+    }
+
+    public function list()
+    {
+        $subcategory = Subcategory::with('category')->paginate(10);
+
+        return SubcategoryResource::collection($subcategory);
     }
 }
